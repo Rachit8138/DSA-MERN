d3c39d61f431f863cf404957e0cc958a55cda3ed
rachit8138 <rachitpra@gmail.com>
2025-02-03 15:46:56 +0545

    commit

--- JS/D16/hw.js
+++ JS/D16/hw.js
@@ -11,7 +11,14 @@ console.log(array.splice(0,n));
 //     Print, [9, 0, -2]
 console.log(array.splice(array.length-n));
 //     Qs3. Write a JavaScript program to check whether a string is blank or not.
-    
+let str=prompt("please enter a string");
+if (str.lenth==0){
+    if (str.length==0){
+        console.log("string is empty");
+    }else{
+        console.log("string is not empty");
+    }
+}
 //     Qs4. Write a JavaScript program to test whether the character at the given (character)
 //     index is lower case.
     
