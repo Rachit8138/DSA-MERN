12dddc96e0933696aeac4fea1980d8e3308cc2b7
rachit8138 <rachitpra@gmail.com>
2025-02-06 08:59:08 +0545

    qs5

--- JS/D19(function)/hw.js
+++ JS/D19(function)/hw.js
@@ -3,9 +3,9 @@
 let arr = [8, 9, 10, 1, 2, 3, 4, 5, 6, 7];
 let num = 5;
 
-function larger(arr,num){
-    for (i=0; i<arr.length; i++){
-        if (arr[i]>num){
+function larger(arr, num) {
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] > num) {
             console.log(arr[i]);
         }
     }
@@ -27,15 +27,15 @@ let str = "abcdabcdefgggh";
     console.log(ans);
 } */
 
-    function getUnique(str){
-        ans='';
-        for (a of str){
-            if(ans.indexOf(a)==-1){
-                ans+=a;
-            }
+function getUnique(str) {
+    ans = '';
+    for (a of str) {
+        if (ans.indexOf(a) == -1) {
+            ans += a;
         }
-        console.log(ans);
     }
+    console.log(ans);
+}
 getUnique(str);
 // Qs3. Write a JavaScript function that accepts a list of country names as input and
 // returns the longest country name as output.
@@ -45,12 +45,12 @@ getUnique(str);
 
 let country = ["Australia", "Germany", "United States of America"]
 
-function size(country){
-    let largest='';
-    for (str of country){
-       if (largest.length<str.length){
-        largest=str;
-       }         
+function size(country) {
+    let largest = '';
+    for (str of country) {
+        if (largest.length < str.length) {
+            largest = str;
+        }
     }
     console.log(largest);
 }
@@ -58,12 +58,12 @@ function size(country){
 size(country);
 // Qs4. Write a JavaScript function to count the number of vowels in a String
 // argument.
-function vowel(str){
-    let count=0;
-    let list=['a','e','i','o','u'];
-    for (x of str){
+function vowel(str) {
+    let count = 0;
+    let list = ['a', 'e', 'i', 'o', 'u'];
+    for (x of str) {
         // if (list.indexOf(x)==0){ // this will check only if present in index 0  
-        if (list.indexOf(x)!=-1){
+        if (list.indexOf(x) != -1) {
             console.log(x);
             count++;
         }
@@ -73,9 +73,10 @@ function vowel(str){
 
 vowel('australia')
 
-
-
-
 // Qs5. Write a JavaScript function to generate a random number within a range
 // (start, end).
-
+function generator(start, end){
+    diff=end - start;
+    console.log(Math.floor(Math.random()*diff)+start);
+}
+generator(100,200)
