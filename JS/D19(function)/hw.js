8ead9b58ecc8f6c96fdc33f314c4dbee0177a6bd
rachit8138 <rachitpra@gmail.com>
2025-02-06 08:12:03 +0545

    qs1

--- JS/D19(function)/hw.js
+++ JS/D19(function)/hw.js
@@ -1,6 +1,17 @@
 // JS (Part 6)
 // Qs1. Write a JavaScript function that returns array elements larger than a number.
+let arr = [8, 9, 10, 1, 2, 3, 4, 5, 6, 7];
+let num = 5;
 
+function larger(arr,num){
+    for (i=0; i<arr.length; i++){
+        if (arr[i]>num){
+            console.log(arr[i]);
+        }
+    }
+}
+
+larger(arr, num)
 // Qs2. Write a JavaScript function to extract unique characters from a string.
 // Example: str = “abcdabcdefgggh” ans = “abcdefgh”
 
