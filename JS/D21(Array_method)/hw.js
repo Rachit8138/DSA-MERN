336df224b4286e8b86a6cd857ae5e59ef255a0f4
rachit8138 <rachitpra@gmail.com>
2025-02-08 23:50:47 +0545

    changes

--- JS/D21(Array_method)/hw.js
+++ JS/D21(Array_method)/hw.js
@@ -16,5 +16,3 @@
 
 // Qs5. Write a function called mergeObjects that accepts two objects and returns a new
 // object which contains all the keys and values of the first object and second object.
-
-
