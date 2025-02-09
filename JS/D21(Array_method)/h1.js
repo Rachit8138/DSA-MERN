94a7265c5adcebb927c071dd9332c58107252163
rachit8138 <rachitpra@gmail.com>
2025-02-09 11:55:09 +0545

    commit

--- JS/D21(Array_method)/h1.js
+++ JS/D21(Array_method)/h1.js
@@ -3,13 +3,28 @@ let arr=[{
     marks:95
 },{
     name:'shradha',
-    marks:94.4
+    marks:94
 }];
 /* forEach */
-arr.forEach((student)=>{
-    console.log(student.marks);
+arr.forEach((el)=>{
+    console.log(el.marks);
 })
 /* Map */
 let gpa =arr.map((el)=>{
     return el.marks/10;
 });
+
+/* Filter */
+
+let value=arr.filter((el)=>{
+    return el.marks%2==0;
+})
+
+let num =[1,2,3,4,7,8,3,23,56,7];// it usually filters out ther required value
+let ans=num.filter((el)=>{
+    return el%2==0;
+});
+
+console.log(value);
+
+/* every */
