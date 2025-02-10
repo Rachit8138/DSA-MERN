11749b9d317cfeaf79abe2ac3818857ff7efbc55
rachit8138 <rachitpra@gmail.com>
2025-02-10 14:04:50 +0545

    commit with the obj literal

---
+++ JS/D21(Array_method)/h3.js
@@ -0,0 +1,25 @@
+let arr=[1,2,3,4,5];
+let newarr=arr;
+newarr.push(7);
+console.log(arr);
+console.log(newarr);
+/* newarr and arr are both same if you change one value then another value change huncha  */
+
+let array=[4,5,6,7,8];
+let newarray= [...array];
+newarray.push(7);
+console.log(array);
+/* only copyting the value  */
+
+console.log(..."hello");
+// individual character is printed
+
+// to combine both in one 
+let nums = [...arr, ...array] ;
+console.log(nums);
+console.log(nums.sort());
+console.log(nums);
+// .sort() will make the new string and change the value of nums
+
+/* ----------------------------------------------------------------------- */
+/* spred with object literals */
