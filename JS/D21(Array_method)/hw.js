fea7d4270d9b3be66b8bbbe89ee4af23079b235b
rachit8138 <rachitpra@gmail.com>
2025-02-14 14:07:50 +0545

    q1

--- JS/D21(Array_method)/hw.js
+++ JS/D21(Array_method)/hw.js
@@ -3,6 +3,15 @@
 
 // Qs1. Square and sum the array elements using the arrow function and then find the
 // average of the array.
+let nums=[1,2,3,4,5];
+console.log(nums.map(el=>{
+    return el*el
+}));
+
+let sum=nums.reduce((sum,el)=>sum+el);
+console.log(sum);
+
+console.log("average: ", sum/nums.length);
 
 // Qs2. Create a new array using the map function whose each element is equal to the
 // original element plus 5.
