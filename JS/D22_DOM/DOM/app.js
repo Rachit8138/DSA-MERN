7babfc25da35fc1a33c425d77288f9f15b763d0b
rachit8138 <rachitpra@gmail.com>
2025-03-28 12:29:40 +0545

    commited

--- JS/D22_DOM/DOM/app.js
+++ JS/D22_DOM/DOM/app.js
@@ -1,14 +1,20 @@
-// let smallImages=document.getElementsByClassName("images");
-// for(let i=0;i<smallImages.length;i++){
-//     console.log(smallImages[i][src])
-// }
+/* //getElementById
+console.log(document.getElementById("description"));//->p#description which is an object that is returned 
+ */
 
-// for (let i=0;i<smallImages.length;i++){
-//     smallImages[i].src="Assets/Spider-Man.png";
-//     console.log(`value of image ${i} is changed`);
-// }
+/* 
+// getElementsByClassName
+let smallImages=document.getElementsByClassName("oldimg");
+for(let i=0;i<smallImages.length;i++){
+    console.log(smallImages[i].src)// [i] will give me the index
+}
+
+for (let i=0;i<smallImages.length;i++){
+    smallImages[i].src="Assets/Spider-Man.png";
+    console.log(`value of image ${i} is changed`);
+} */
 
 /* Query */
-console.log(Document.querySelector("h1"));
+// console.log(Document.querySelector("h1"));
 
 
