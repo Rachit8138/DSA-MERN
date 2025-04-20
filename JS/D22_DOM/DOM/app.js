1c2707650b47c7175fb55af0d9f3f7eee901e149
rachit8138 <rachitpra@gmail.com>
2025-04-20 10:53:24 +0545

    commit

--- JS/D22_DOM/DOM/app.js
+++ JS/D22_DOM/DOM/app.js
@@ -24,36 +24,25 @@ console.log(document.getElementsByTagName("p"));// p p
 
 /* Query */
 console.log(document.querySelector("h1"));
+console.log(document.querySelector("h1").textContent);
 console.log(document.querySelector("#description"));
 console.log(document.querySelector(".oldimg"));
 console.log(document.querySelector("div a"));// will give you the first element found in div of 'a' element
 console.log(document.querySelectorAll("p"));// gives you all the element 'p'
 
 /* Using properties and method */
-/* let para = document.getElementById('p')
-para.innerText='abc';
-'abc'   
-para.innerHTML="hi, i am <b> Peter parker</b>";
-'hi, i am <b> Peter parker</b>'
- */
+
+console.log(document.querySelector("p").innerHTML);
 
 let heading = document.querySelector('h1');
-console.dir(heading);// to display the properties of h1
-console.log(heading);// to display the value of the heading
-
 // heading.innerHTML="<u> Spiderman </u>";
+console.dir(heading);
 heading.innerHTML=`<u> ${heading.innerText}</u>`
+console.log(heading.innerHTML);
 /* 
 new chapter
 manipulating attributes
 */
-
-let img =document.querySelector('img')
-console.dir(img);// displays the property 
-
-let headings =document.querySelector('h1')
-console.log(headings);
-
 
 /* very imp qn */
 // change all the anchor ag into blue
@@ -75,16 +64,15 @@ Manipulating style */
 
 let headerr =document.querySelector('h1')
 console.log(headerr.classList);
-headerr.classList.add("green");// this will make the changes in the file and giv you the output and yesko bhitra bhaeko classes
-headerr.classList.remove("green");// removes
-headerr.classList.contains("green");// gives true or false
-console.log(headerr.classList.toggle("green"));
-console.log(headerr.classList.toggle("green"));
-
+headerr.classList.add("brown");// this will make the changes in the file and giv you the output and yesko bhitra bhaeko classes
+headerr.classList.remove("brown");// removes
+headerr.classList.contains("brown");// gives true or false
+console.log(headerr.classList.toggle("brown"));
+console.log(headerr.classList.toggle("brown"));
+// 
 /* new 
 Navigation
 */
-
 let h4=document.querySelector('h4');
 console.log(h4.parentElement);
 let box =document.querySelector(".box");
@@ -108,3 +96,8 @@ let body=document.querySelector('body');
 console.log(body.appendChild(newP));
 newP.append("this is new append")
 console.log(newP);
+
+
+/* did this myself where i choosed about which is in h2 and made it bold */
+ab=document.querySelector('h2')
+ab.innerHTML=`<u> ${heading.innerText}</u>`
