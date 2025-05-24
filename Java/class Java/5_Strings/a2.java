5c0391bff27726653802ccba62f15feaa2db686d
rachit8138 <rachitpra@gmail.com>
2025-05-24 14:59:14 +0545

    commited

--- Java/class Java/5_Strings/a2.java
+++ Java/class Java/5_Strings/a2.java
@@ -14,19 +14,15 @@ public class a2 {
         
     }
     
-    public static void Anagram(String str, String str2){
-
-
-    }
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("enter a word: ");
-        String str=sc.nextLine();
+
         System.out.print("enter a word: ");
         String str2=sc.nextLine();
+        
+        countVowel(str2);
         sc.close();
-        countVowel(str);
-        Anagram(str, str2);   //frequency of each character in both the string same
+        // Anagram(str, str2);   //frequency of each character in both the string same
 
     }
 }
