7148416fd3ad0e5d869b3c132ec752a2b2fb49e1
rachit8138 <rachitpra@gmail.com>
2025-08-12 16:54:20 +0545

    commited

---
+++ mern/l1.js
@@ -0,0 +1,50 @@
+function dosomethign(){
+
+}
+
+const dosomethign=()=>{
+    
+}
+
+export default function dosomethign(){
+
+}
+
+export const dosomethign=()=>{
+    
+}
+// react 
+const MyComponoent = () =>{
+    return <div></div>
+}
+
+// anonymous function 
+<button 
+    onClick={()=>{
+        console.log("hello world");
+    }}
+></button>
+
+// working in conditionals using ternary operator 
+// write consditionals and logic in the html part not in the separate file 
+
+let age = 10;
+// TAKING way to much space
+
+if(age= 10){
+    name ="PEDRO";
+}else{
+    name = "m"
+}
+/* using ternary operators  */
+let n= 14; 
+let a =n>10 && "pedro"//if this condition is true do this 
+let a2 =n>10 || "rachit"// if this condition is not ture do this operation 
+let a3 = n>10?"pedro":"rachit"
+// you will be returning in the html
+/* Using ternary operator  */
+
+const my=() =>{
+    return age>10?<div>pedro</div>?<div> jack </div>
+}
+// object are called dictionary in python and hash map in java
