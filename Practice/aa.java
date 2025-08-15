9c90195467706c4081f98fe70a15852d3a1809dc
rachit8138 <rachitpra@gmail.com>
2025-08-15 10:40:04 +0545

    commited

---
+++ Practice/aa.java
@@ -0,0 +1,24 @@
+package Practice;
+
+public class aa {
+    public static class Node{
+        int data; 
+        Node left; 
+        Node right; 
+        public Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null; 
+        }
+    }
+
+    static int idx =-1;
+    public static class BinaryTree{
+
+    }
+    public static void main(String[] args) {
+        int 
+        
+    }
+    
+}
