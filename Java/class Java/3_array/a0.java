90de1535ea4dbd6d4b422e052d4cdb66009af039
Rachit Prajapati <rachitpra@gmail.com>
2026-01-14 10:42:39 +0545

    commited

--- Java/class Java/3_array/a0.java
+++ Java/class Java/3_array/a0.java
@@ -1,32 +1,21 @@
+import java.util.Arrays;
+
 public class a0 {
 
-    public static void main(String args[]) {
-        int height[] = { 4, 2, 0, 3, 2, 5 };
-        System.out.println(trapWater(height));
+    class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int arr[]=new int[m+n];
+        for(int i=0;i<m;i++){
+            arr[i]=nums1[i];
+        }
+        for(int j=m;j<m+n;j++){
+            arr[j]=nums2[j];
+        }
+        
+        Arrays.sort(nums1);
+        return arr;
+        
     }
-
-    public static int trapWater(int height[]) {
-        int n = height.length;
-        int leftmax[] = new int[n];
-        int rightmax[] = new int[n];
-        leftmax[0] = height[0];
-        rightmax[n - 1] = height[n - 1];
-
-        // for left max
-        for (int i = 1; i < n; i++) {
-            leftmax[i] = Math.max(leftmax[i - 1], height[i]);
-        }
-        // for right max
-        for (int j = n - 2; j >=0; j--) {
-            rightmax[j] = Math.max(rightmax[j + 1], height[j]);
-        }
-
-        int water = 0;
-        for (int k = 0; k < n; k++) {
-            water += Math.min(leftmax[k], rightmax[k]) - height[k];
-        }
-        return water;
-
-    }
+}
 
 }
